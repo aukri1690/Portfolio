a0d5842af6d018d0b01979e899441112818b1420
aukri1690 <toro369@icloud.com>
2025-09-17 13:42:07 +0900

    feat: implement navigation button

---
+++ src/components/NavigationMenu.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { Button, Flex } from "@chakra-ui/react"
+import { FiGithub } from "react-icons/fi";
+import { AiOutlineSpotify } from "react-icons/ai";
+
+const NavigationMenu = () => {
+  const router = useRouter();
+  return (
+    <Flex justify='center' gap={16}>
+      <Button onClick={() => router.push('/github')} variant='outline' size='lg' rounded='md' colorPalette='purple'>GitHub Chart<FiGithub /></Button>
+      <Button onClick={() => router.push('/spotify')} variant='outline' size='lg' rounded='md' colorPalette='green'>My Spotify<AiOutlineSpotify /></Button>
+    </Flex>
+  );
+};
+
+export default NavigationMenu;
