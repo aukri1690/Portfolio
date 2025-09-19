6b0b70aafe1616066d53f0d35fba7234271308dd
aukri1690 <toro369@icloud.com>
2025-09-19 19:10:18 +0900

    feat: implement tooltip of tech stack name

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Tooltip } from "@/components/ui/tooltip"
 import { Text, Image, Icon, Flex } from "@chakra-ui/react"
 import { SiTypescript } from "react-icons/si";
 import { FaReact } from "react-icons/fa";
@@ -13,25 +14,43 @@ import { RiNotionFill } from "react-icons/ri";
 
 const Profile = () => {
   return (
-      <Flex align='center' direction='column' >
-        <Image src='/icon.jpg' alt='Profile Icon' boxSize='250px' borderRadius='full' fit='cover' mt={2} />
-        <Text mt={3} fontSize='lg'>-- Profile --</Text>
-        <Text mt={3}>永田浬久</Text>
-        <Text mt={1}>2005年1月16日</Text>
-        <Text mt={1}>名古屋市立大学 データサイエンス学部 3年</Text>
-        <Text mt={3} fontSize='lg'>-- Tech Stack --</Text>
-        <Flex align='center' direction='row' gap={3} mt={4}>
+    <Flex align='center' direction='column' >
+      <Image src='/icon.jpg' alt='Profile Icon' boxSize='250px' borderRadius='full' fit='cover' mt={2} />
+      <Text mt={3} fontSize='lg'>-- Profile --</Text>
+      <Text mt={3}>永田浬久</Text>
+      <Text mt={1}>2005年1月16日</Text>
+      <Text mt={1}>名古屋市立大学 データサイエンス学部 3年</Text>
+      <Text mt={3} fontSize='lg'>-- Tech Stack --</Text>
+      <Flex align='center' direction='row' gap={3} mt={4}>
+        <Tooltip content="TypeScript">
           <Icon as={SiTypescript} boxSize={9} />
+        </Tooltip>
+        <Tooltip content="React">
           <Icon as={FaReact} boxSize={9} />
+        </Tooltip>
+        <Tooltip content="Next.js">
           <Icon as={RiNextjsLine} boxSize={9} />
+        </Tooltip>
+        <Tooltip content="GraphQL">
           <Icon as={GrGraphQl} boxSize={9} />
+        </Tooltip>
+        <Tooltip content="Chakra UI">
           <Icon as={SiChakraui} boxSize={9} />
+        </Tooltip>
+        <Tooltip content="GitHub">
           <Icon as={BsGithub} boxSize={9} />
+        </Tooltip>
+        <Tooltip content="GitHub Copilot">
           <Icon as={SiGithubcopilot} boxSize={9} />
+        </Tooltip>
+        <Tooltip content="ChatGPT">
           <Icon as={SiOpenai} boxSize={9} />
+        </Tooltip>
+        <Tooltip content="Notion">
           <Icon as={RiNotionFill} boxSize={9} />
-        </Flex>
+        </Tooltip>
       </Flex>
+    </Flex>
   );
 };
 
