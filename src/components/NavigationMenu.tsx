26315b46b93e9b8c00cfcfed96b0278cac7ce5f0
aukri1690 <toro369@icloud.com>
2025-09-19 16:29:49 +0900

    refactor: change structure about github-chart

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -8,7 +8,7 @@ const NavigationMenu = () => {
   const router = useRouter();
   return (
     <Flex mt={10} >
-      <Button onClick={() => router.push('/github')}  size='lg' rounded='md' colorPalette='purple'>GitHub Chart<FiGithub /></Button>
+      <Button onClick={() => router.push('/github-chart')}  size='lg' rounded='md' colorPalette='purple'>GitHub Chart<FiGithub /></Button>
     </Flex>
   );
 };
