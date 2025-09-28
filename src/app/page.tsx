796af7af2aca8d544dc8bca4f6e62561601d5dac
aukri1690 <toro369@icloud.com>
2025-09-28 11:15:08 +0900

    feat: implement router to github button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Flex } from "@chakra-ui/react"
 import Profile from "@/components/Profile";
 import BackGroundVideo from "@/components/BackgroundVideo";
+import RouterButton from "@/components/RouterButton";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <BackGroundVideo />
       <Flex justify='center' align='center' direction='column' minH='100vh'>
       <Profile />
+      <RouterButton />
       </Flex>
     </>
   );
